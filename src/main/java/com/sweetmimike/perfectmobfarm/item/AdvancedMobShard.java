30aa1410fabbeddc738565372d02e614f062c5d0
Aurelien Carpentier <aurecarp@live.fr>
2022-06-08 23:02:30 +0200

    Comment

--- src/main/java/com/sweetmimike/perfectmobfarm/item/AdvancedMobShard.java
+++ src/main/java/com/sweetmimike/perfectmobfarm/item/AdvancedMobShard.java
@@ -1,5 +1,8 @@
 package com.sweetmimike.perfectmobfarm.item;
 
+/**
+ * Advanced Mob Shard
+ */
 public class AdvancedMobShard extends MobShard {
     public AdvancedMobShard(Properties pProperties) {
         super(pProperties);
